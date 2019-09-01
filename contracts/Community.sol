7263a200945c9474d3c923ad19939a7005a2f2ca
Tommy Cox <tommy.cox@protonmail.com>
2019-09-01 04:32:22 -0600

    added Network contract to track communities

--- contracts/Community.sol
+++ contracts/Community.sol
@@ -1,29 +1,28 @@
 pragma solidity 0.4.26;
 
 contract Community {
+    address public network;
     address public owner;
-    string public name;
+    string public title;
     string public tokenName;
     string public tokenSymbol;
     int public initialPrice;
     string public benefit;
 
-    event Created(string name);
-
     constructor(
-        string _name,
+        address _owner,
+        string _title,
         string _tokenName,
         string _tokenSymbol,
         int _initialPrice,
         string _benefit
     ) public {
-        owner = msg.sender;
-        name = _name;
+        network = msg.sender;
+        owner = _owner;
+        title = _title;
         tokenName = _tokenName;
         tokenSymbol = _tokenSymbol;
         initialPrice = _initialPrice;
         benefit = _benefit;
-
-        emit Created(name);
     }
 }
--- test/contracts/Community.test.js
+++ test/contracts/Community.test.js
@@ -1,3 +1,4 @@
+const Network = artifacts.require("Network");
 const Community = artifacts.require("Community");
 
 const truffleAssert = require("truffle-assertions");
@@ -7,34 +8,46 @@ require("chai")
   .should();
 
 contract("Community", ([deployer]) => {
+  let network;
+  let result;
   let community;
 
   beforeEach(async () => {
-    community = await Community.new(
-      "Wild Water", // Name
-      "Aqua", // Token Name
-      "AQUA", // Token Symbol
+    network = await Network.new();
+    result = await network.createCommunity(
+      "Wild Water", // Title
+      "Waves", // Token Name
+      "WAVES", // Token Symbol
       200, // Initial Price
-      "Access to the water park for 1 month!" // Benefit
+      "Access to the water park for 1 month!", // Benefit
+      {
+        from: deployer // Owner
+      }
     );
+    const [communityAddress] = await network.getAllCommunities();
+    community = await Community.at(communityAddress);
   });
 
   describe("creation", () => {
+    it("tracks the network", async () => {
+      const communityNetwork = await community.network();
+      communityNetwork.should.equal(network.address);
+    });
     it("tracks the owner", async () => {
       const owner = await community.owner();
       owner.should.equal(deployer);
     });
-    it("tracks the name", async () => {
-      const name = await community.name();
-      name.should.equal("Wild Water");
+    it("tracks the title", async () => {
+      const title = await community.title();
+      title.should.equal("Wild Water");
     });
     it("tracks the token name", async () => {
       const tokenName = await community.tokenName();
-      tokenName.should.equal("Aqua");
+      tokenName.should.equal("Waves");
     });
     it("tracks the token symbol", async () => {
       const tokenSymbol = await community.tokenSymbol();
-      tokenSymbol.should.equal("AQUA");
+      tokenSymbol.should.equal("WAVES");
     });
     it("tracks the initial price", async () => {
       const initialPrice = await community.initialPrice();
@@ -44,13 +57,9 @@ contract("Community", ([deployer]) => {
       const benefit = await community.benefit();
       benefit.should.equal("Access to the water park for 1 month!");
     });
-    it("emits a Created event with the community name", async () => {
-      const result = await truffleAssert.createTransactionResult(
-        community,
-        community.transactionHash
-      );
-      truffleAssert.eventEmitted(result, "Created", event => {
-        return event.name === "Wild Water";
+    it("emits a CommunityCreated event with the community title", async () => {
+      truffleAssert.eventEmitted(result, "CommunityCreated", community => {
+        return community.title === "Wild Water";
       });
     });
   });
