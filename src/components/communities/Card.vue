50e9bc9687a111dbc44bb54dbc551d20b2c6acbe
Tommy Cox <tommy.cox@protonmail.com>
2019-09-02 17:32:32 -0600

    Refactor community card

---
+++ src/components/communities/Card.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-card class="full-width">
+    <q-card-section>
+      <h6 class="q-my-xs">
+        <strong>{{ community.name }}</strong>
+      </h6>
+      <p>
+        Token: {{ community.tokenName }}
+        <br />
+        Symbol: {{ community.tokenSymbol }}
+        <br />
+        Initial Price: {{ community.tokenInitialPrice }}
+        <br />
+        Benefit: {{ community.benefit }}
+        <br />
+      </p>
+      <div class="row justify-center q-gutter-md">
+        <router-link :to="`communities/${community.address}`">
+          <q-btn>Detail</q-btn>
+        </router-link>
+        <q-btn>Join</q-btn>
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  props: {
+    community: {
+      type: Object
+    }
+  }
+};
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,46 +1,25 @@
 <template>
+  <!-- eslint-disable -->
   <q-page class="flex flex-center column">
     <h2>Communities</h2>
-    <div
-      v-for="(community, index) in communities"
-      :key="index"
-      :community="community"
-      class="col-12 q-pa-lg"
-    >
-      <q-card class="full-width">
-        <q-card-section>
-          <h6 class="q-my-xs">
-            <strong>{{ community.name }}</strong>
-          </h6>
-          <p>
-            Token: {{ community.tokenName }}
-            <br />
-            Symbol: {{ community.tokenSymbol }}
-            <br />
-            Initial Price: {{ community.tokenInitialPrice }}
-            <br />
-            Benefit: {{ community.benefit }}
-            <br />
-          </p>
-          <div class="row justify-center q-gutter-md">
-            <router-link :to="`communities/${community.address}`">
-              <q-btn>Detail</q-btn>
-            </router-link>
-            <q-btn>Join</q-btn>
-          </div>
-        </q-card-section>
-      </q-card>
+    <div v-for="(community, index) in communities" :key="index" class="col-12 q-pa-lg">
+      <CommunityCard :community="community" />
     </div>
     <q-btn fab color="primary" size="xl" icon="add" to="/communities/create" />
   </q-page>
 </template>
 
 <script>
+import CommunityCard from "src/components/communities/Card";
+
 export default {
   computed: {
     communities() {
       return this.$store.getters["communities/all"];
     }
+  },
+  components: {
+    CommunityCard
   }
 };
 </script>
