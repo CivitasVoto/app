1ef66d330c1f8708e5f707c9e3fb2e24b9f8caf3
Tommy Cox <tommy.cox@protonmail.com>
2019-09-11 19:30:25 -0600

    Fix tests and test SmartToken

--- test/contracts/communiteths.test.js
+++ test/contracts/communiteths.test.js
@@ -1,5 +1,6 @@
 const Network = artifacts.require("Network");
 const Community = artifacts.require("Community");
+const SmartToken = artifacts.require("SmartToken");
 
 const truffleAssert = require("truffle-assertions");
 
@@ -7,28 +8,35 @@ require("chai")
   .use(require("chai-as-promised"))
   .should();
 
-contract("Community", ([deployer]) => {
+contract("Network", ([deployer]) => {
+  // Cross-test instance variables
   let network;
-  let result;
+  let result; // Transaction receipt from creating community
   let community;
+  let token;
 
-  beforeEach(async () => {
+  before(async () => {
+    // Instantiate network.
     network = await Network.new();
+
+    // Create community.
     result = await network.createCommunity(
       "Wild Water", // Community Name
+      "Access to the water park for 1 month!", // Benefit
       "Waves", // Token Name
       "WAVES", // Token Symbol
-      200, // Token Initial Price
-      "Access to the water park for 1 month!", // Benefit
       {
         from: deployer // Owner
       }
     );
-    const [communityAddress] = await network.getAllCommunities();
+    const [communityAddress] = await network.getCommunities();
+
+    // Get community and token instances.
     community = await Community.at(communityAddress);
+    token = await SmartToken.at(await community.token());
   });
 
-  describe("creation", () => {
+  describe("community creation", () => {
     it("tracks the owner", async () => {
       const owner = await community.owner();
       owner.should.equal(deployer);
@@ -37,25 +45,25 @@ contract("Community", ([deployer]) => {
       const name = await community.name();
       name.should.equal("Wild Water");
     });
-    it("tracks the token name", async () => {
-      const tokenName = await community.tokenName();
-      tokenName.should.equal("Waves");
-    });
-    it("tracks the token symbol", async () => {
-      const tokenSymbol = await community.tokenSymbol();
-      tokenSymbol.should.equal("WAVES");
-    });
-    it("tracks the initial price", async () => {
-      const tokenInitialPrice = await community.tokenInitialPrice();
-      tokenInitialPrice.toString().should.equal("200");
-    });
     it("tracks the benefit", async () => {
       const benefit = await community.benefit();
       benefit.should.equal("Access to the water park for 1 month!");
     });
-    it("emits a CommunityCreated event with the community title", async () => {
-      truffleAssert.eventEmitted(result, "CommunityCreated", community => {
-        return community.name === "Wild Water";
+    it("tracks the token name", async () => {
+      const tokenName = await token.name();
+      tokenName.should.equal("Waves");
+    });
+    it("tracks the token symbol", async () => {
+      const tokenSymbol = await token.symbol();
+      tokenSymbol.should.equal("WAVES");
+    });
+    it("emits a CommunityCreated event with the community information", async () => {
+      truffleAssert.eventEmitted(result, "CommunityCreated", event => {
+        return (
+          event.network === network.address &&
+          event.community === community.address &&
+          event.token === token.address
+        );
       });
     });
   });
